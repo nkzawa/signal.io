0f0428d9943a6783fc8eb48ae0234eeeb0b91ee3
Naoyuki Kanezawa <naoyuki.kanezawa@gmail.com>
2013-08-01 23:15:01 +0900

    don't match to multiple routes

--- test/router.js
+++ test/router.js
@@ -5,6 +5,7 @@ var expect = require('chai').expect
   , client = support.client
   , Router = signal.Router;
 
+
 describe('router', function() {
   beforeEach(function(done) {
     this.router = new Router();
@@ -12,48 +13,62 @@ describe('router', function() {
   })
   afterEach(support.stopServer);
 
-  describe('.match(nsp, i)', function() {
-    it('should match based on index', function() {
+  describe('.match(nsp)', function() {
+    it('should match (0)', function() {
       var router = this.router;
       router.route('/foo', function(){});
-      router.route('/foob?', function(){});
-      router.route('/bar', function(){});
 
-      var nsp = '/foo';
-
-      var route = router.match(nsp, 0);
+      var route = router.match('/foo');
       expect(route.constructor.name).to.eql('Route');
       expect(route.path).to.eql('/foo');
+    });
 
-      var route = router.match(nsp, 1);
+    it('should match (1)', function() {
+      var router = this.router;
+      router.route('/foob?', function(){});
+
+      var route = router.match('/foo');
       expect(route.path).to.eql('/foob?');
+    });
 
-      var route = router.match(nsp, 2);
+    it('should match (2)', function() {
+      var router = this.router;
+      router.route('/bar', function(){});
+
+      var route = router.match('/foo');
       expect(route).to.not.be.ok;
 
-      nsp = '/bar';
-      var route = router.match(nsp);
+      var route = router.match('/bar');
       expect(route.path).to.eql('/bar');
     });
   });
 
-  describe('.matchRequest(socket, i)', function() {
-    it('should match based on index', function() {
+  describe('.matchRequest(socket)', function() {
+    it('should match (0)', function() {
       var router = this.router;
       router.route('/foo', function(){});
+      var socket = {nsp: {name: '/foo'}};
+
+      var route = router.matchRequest(socket);
+      expect(route.constructor.name).to.eql('Route');
+      expect(route.path).to.eql('/foo');
+    });
+
+    it('should match (1)', function() {
+      var router = this.router;
       router.route('/foob?', function(){});
+      var socket = {nsp: {name: '/foo'}};
+
+      var route = router.matchRequest(socket);
+      expect(route.path).to.eql('/foob?');
+    });
+
+    it('should match (2)', function() {
+      var router = this.router;
       router.route('/bar', function(){});
       var socket = {nsp: {name: '/foo'}};
 
-      var route = router.matchRequest(socket, 0);
-      expect(route.constructor.name).to.eql('Route');
-      expect(route.path).to.eql('/foo');
-
-      var route = router.matchRequest(socket, 1);
-      expect(socket._route_index).to.eql(1);
-      expect(route.path).to.eql('/foob?');
-
-      var route = router.matchRequest(socket, 2);
+      var route = router.matchRequest(socket);
       expect(route).to.not.be.ok;
 
       socket.nsp.name = '/bar';
